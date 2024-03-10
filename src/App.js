c647fc183b7d826102245685dd1f718531bad7a4
codeRastogi <harshit1122ras@gmail.com>
2024-03-10 22:42:59 +0530

    The End

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
 import Testimonial from './components/Testimonial';
-
+import reviews from './data';
 function App() {
   return (
-    <div className="App">
-      <div>
-        <h1>Our Testimonials</h1>
-        <div></div>
-        <Testimonial/>
+    <div className='flex flex-col w-[100vw] h-[100vh] justify-center items-center bg-gray-200'>
+      <div className='text-center'>
+        <h1 className='text-4xl font-bold'>Our Testimonials</h1>
+        <div className='bg-violet-400 h-[4px] w-1/5 mt-1 mx-auto'></div>
+        <Testimonial reviews = {reviews}/>
       </div>
     </div>
   );
